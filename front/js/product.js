af21f7ad57882bfb7933d98d3c180872dad50461
Celine Clovel <c.clovel@gmail.com>
2022-07-28 19:54:31 -0400

    Simplification du code et suppression des fonctions mises en commentaire - Affichage de l'objet 'productValues' avec l'ID, la couleur et la quantité dans le localStorage

--- front/js/product.js
+++ front/js/product.js
@@ -21,8 +21,8 @@ async function getDataProduct(_dataProduct) {
     .then((product)=> {
         console.log(product);
         showOneProduct(product);
-        saveProductInLocalStorage(product);
-        addProductToCart();
+        // saveProductInLocalStorage(product);
+        // addToCart();
         // clearLocalStorage(product);
 
     })
@@ -67,96 +67,25 @@ function showOneProduct(product) {
 
 //*************   CART    *************/
 
-// CREATE VARIABLES PRODUCT & CART TO GET THE ITEM IN THE LOCALSTORAGE
+// CREATE VARIABLE CART TO GET THE ITEM IN THE LOCALSTORAGE 
 
-let products = JSON.parse(localStorage.getItem("products"));
-let cart = JSON.parse(localStorage.getItem("cart"));
-
-// SAVE THE CART IN THE LOCALSTORAGE
-
-function saveProductInLocalStorage(product) {
-    localStorage.setItem("products", JSON.stringify(product));
-  
-    // Verify if the cart array is in the localStorage. If not, the cart is stored
-    if (localStorage.getItem("cart") == null){
-         localStorage.setItem("cart", "[]");
-     }
-}
-
-//FIND A PRODUCT BY ITS ID AND ADD IT IN THE CART 
-
-//This function return the Id of the product found in products 
-function addToCart(productId) {
-    let product = products.find(function(product) {
-        return product.id == productId;
-    });
-
-    // If the cart is empty, the push method put a product in the cart
-    if(cart.length == null) {
-        cart.push(product);
-    }
-    else {
-        // if the product exist, the find method return the product by its Id and
-        // store it in the variable "res" 
-        let res = cart.find(element => element.id == productId);
-        // If the product doesn't exist, the find method return the value undefined    
-        if(res == undefined) {
-            // if the value is indefined, the push method put a product in the cart
-            cart.push(product);
-        }
-    }
-    saveProductInLocalStorage();
-}
-
-// FUNCTION TO CLEAR THE LOCALSTORAGE
-
-function clearLocalStorage(productId) {
-    localStorage.clear(productId)
-}
-
-// FUNCTION TO TEST AND GET THE COLOR VALUE OF THE PRODUCT
-        
-function getColorValue() {
-    const colorOption = document.querySelector("#colors").value;
-
-    if (colorOption == null || colorOption === "" ) {
-        alert("Vous n'avez pas choisi de couleur");
-    } else { 
-        return colorOption;
-    }
-}
-
-// FUNCTION TO TEST AND GET THE QUANTITY VALUE OF THE PRODUCT
-
-function getQuantityValue() {
-    const quantity = document.querySelector("#quantity").value;
-
-    if (quantity == 0) {
-        alert("Vous n'avez pas choisi une quantité");
-    } else if (quantity > 100) {
-        alert("Vous ne pouvez pas commander plus de 100 exemplaires");
-        quantity == 0;
-    } else {
-        return parseInt(quantity);
-    }
-}
+let item = JSON.parse(localStorage.getItem("productValues"));
 
 // PUT AN ADDEVENTISLISTENER ON THE BUTTON
     let btnAddToCart = document.querySelector("#addToCart");
-    btnAddToCart.addEventListener("click", (colorOption, quantity, id)=>  {
+    btnAddToCart.addEventListener("click", ()=>  {
 
-        getColorValue();
-        getQuantityValue()
+        const colorOption = document.querySelector("#colors").value;
+        const quantity = document.querySelector("#quantity").value;
 
-        let product = {
-            color: colorOption,
-            quantity: quantity,
+
+        let productValues = {
             id: id,
+            color: colorOption,
+            quantity: quantity
         };
 
-        addToCart(product);
+        localStorage.setItem("productValues", JSON.stringify(productValues));
     });
-
-
 
 getDataProduct();                     
